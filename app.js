1bece24ca2ece74aae21908b1cc9b1420e43d88d
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-09 11:52:24 +0500

    #fix #change app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+//bot token
+
+var telegram_bot_id = "teken"; // 'token'ni o'rniga Siz yaratgan Bot tokenini yozing
+
+//chat id
+var chat_id = 44; // '44'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing
+var u_name, email, message;
+var ready = function() {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Ismi: " + u_name + "\nEmail: " + email + "\nIzoh: " + message;
+};
+var sendtelegram = function() {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
